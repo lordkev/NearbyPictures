33ad6660dfe7d6738af13f79e90a6fd4c9b1ac8a
Kevin Lord <kevin@gowalla.com>
2011-12-01 16:33:33 -0600

    Initial Commit

---
+++ NearbyPictures/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NearbyPictures
+//
+//  Created by Kevin Lord on 12/1/11.
+//  Copyright (c) 2011 Kapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NearbyPictures/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NearbyPictures
+//
+//  Created by Kevin Lord on 12/1/11.
+//  Copyright (c) 2011 Kapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
